83950e90b84ac3b923bdcd8b68bf0bd80002d986
octavieploye <octavie.ploye@gmail.com>
2023-04-14 22:47:04 +0200

    add & remove product from cart fixed :)

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,76 +1,82 @@
 'use client';
 
 import Image from "next/image";
-import { useCartStore }from "@/store";
-import formatPrice from "@/Utility/PriceFormat"
-import { IoAddCircle, IoRemoveCircle } from "react-icons/io5"
+import { useCartStore } from "@/store";
+import formatPrice from "@/Utility/PriceFormat";
+import {IoAddCircle, IoRemoveCircle} from 'react-icons/io5';
 import basket from "@/public/basket.png"
 import { motion, AnimatePresence } from "framer-motion"
-
 
 export default function Cart() {
     const cartStore = useCartStore();
     console.log(cartStore.isOpen)
 
-    // Total price of all items in the cart
-    // acc = accumulator
-    const totalPrice = cartStore.cart.reduce((acc, item) => {
-        return acc + item.unit_amount! * item.quantity!
-    }, 0)
-
+                // Total price of all items in the cart
+                // acc = accumulator
+                        const totalPrice = cartStore.cart.reduce((acc, item) => {
+                            return acc + item.unit_amount! * item.quantity!
+                        }, 0)
 
     return (
-        // We use the isOpen property to toggle the cart
-        // We use the motion.div to animate/fade in & fade out the cart
+
+                // We use the isOpen property to toggle the cart
+                // We use the motion.div to animate/fade in & fade out the cart
         <motion.div 
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        exit={{opacity:0}}
-        onClick={() => cartStore.toggleCart()}  
-        className="fixed w-full h-screen left-0 top-0 bg-black/25">
-            {/* Cart */}
-           <motion.div 
-           layout
-           onClick={(e) =>e.stopPropagation()} 
-           className="bg-gray-100 absolute right-0 top-0 w-full lg:w-2/5 p-12 overflow-y-scroll text-gray-500">
-                <button onClick={() => cartStore.toggleCart()} className="text-sm font-bold pb-12"> Back to Store ☑️ </button>
+                initial={{opacity:0}}
+                animate={{opacity:1}}
+                exit={{opacity:0}}
+                onClick={() => cartStore.toggleCart()}  
+                className="fixed w-full h-screen left-0 top-0 bg-black/25">
+
+            <motion.div
+                 layout
+                 onClick={(e) =>e.stopPropagation()} 
+                 className="bg-gray-100 absolute right-0 top-0 w-full lg:w-2/5 p-12 overflow-y-scroll text-gray-500">
+                    <button onClick={() => cartStore.toggleCart()} className="text-sm font-bold pb-12"> Back to Store ☑️ </button>
+                {/* <h1>Here's your shopping List</h1> */}
                 {cartStore.cart.map((item) => (
                     <motion.div layout key={item.id}   className="flex py-4 gap-4">
-                        <Image className="rounded-md h24"  src={item.image} width={120} height={120} alt={item.name} />
+                        <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={120}
+                            height={120}
+                            className="rounded-md h-24"
+                       />
+
                         <motion.div layout>
-                            <h2 className="mb-4 font-bold" >{item.name}</h2>
-                            {/* update quanity of a product */}
-                            <div className="flex gap-2">
+                            <h2 className="mb-4 font-bold">{item.name}</h2>
+                    {/* UPDATE QUANTITY OF A PRODUCT     */}
+                            <div className="flex gap-2" >
                                 <h2>Quantity: {item.quantity}</h2>
-                                    <button onClick={() => 
-                                        cartStore.removeProduct({
-                                            id: item.name, 
-                                            quantity: item.quantity, 
-                                            name: item.name, 
-                                            image: item.image,
-                                            unit_amount: item.unit_amount,})} >
-                                        <IoRemoveCircle/>
-                                    </button>
-                                    <button onClick={() => 
-                                        cartStore.addProduct({
-                                            id: item.name, 
-                                            quantity: item.quantity, 
-                                            name: item.name, 
-                                            image: item.image,
-                                            unit_amount: item.unit_amount,})} >
-                                                
-                                        <IoAddCircle/>
-                                    </button>
+                                <button onClick={() => cartStore.removeProduct({
+                                    id:item.id,
+                                    name:item.name,
+                                    unit_amount:item.unit_amount, 
+                                    image:item.image, 
+                                    quantity:item.quantity
+                                    })} 
+                                
+                                ><IoRemoveCircle/></button>
+                                <button onClick={() => cartStore.addProduct({
+                                    id:item.id,
+                                    name:item.name,
+                                    unit_amount:item.unit_amount, 
+                                    image:item.image, 
+                                    quantity:item.quantity
+                                    })} ><IoAddCircle/></button>
                             </div>
-                            <p className="text-sm" >Price: {item.unit_amount && formatPrice(item.unit_amount)}  </p>
+                                <p className="text-sm">
+                                    {item.unit_amount && formatPrice(item.unit_amount)}
+                                </p>
+
                         </motion.div>
-                        
                     </motion.div>
                 ))}
-                {/* Checkout and Total */}
+                    {/* Checkout and Total */}
                     {cartStore.cart.length > 0  && (
                 <motion.div layout>
-                <p>Total:     { formatPrice (totalPrice)}</p>
+                <p  className="text-sky-800 text-bold">Total:     { formatPrice (totalPrice)}</p>
                 {/* When basket is not empty */}
                     <button className="py-2 mt-4 bg-sky-700 text-white w-full rounded-md">
                         Checkout
--- store.ts
+++ store.ts
@@ -10,85 +10,63 @@ type CartState = {
     // clearCart: () => void;
     addProduct: (item: AddCartType) => void;
     removeProduct:(item: AddCartType) => void;
-    paymentIntent: string;
-    onCheckout: string;
-    setPaymentIntent: (val: string) => void;
-    setCheckout: (val: string) => void;
+    // paymentIntent: string;
+    // onCheckout: string;
+    // setPaymentIntent: (val: string) => void;
+    // setCheckout: (val: string) => void;
    
 
 }
-//TODO!! Duplicate items in cart when addingPRoduct directly from the cart
-// TODO when adding Product fromt the Add to Cart button qty is not added in the shopping counter. However the quantity shows in the cartView but cannot be removed with the remove Product button
+
 export const useCartStore = create<CartState>() (
     persist(
         // "set" is a function that updates the data in your state
         (set) => ({
             cart: [],
             isOpen: false,
-            paymentIntent: "",
-            onCheckout: "",
             // "state" is the current state of the whole cartStore
                 toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
-
-                // Add Item to Cart
-                addProduct: (item) => set((state) => {
+                        addProduct: (item) => set((state) => {
 
                             // Check if the item already exists in the cart
                             const existingItem = state.cart.find((cartItem) => cartItem.id === item.id);
-                            
                             if (existingItem) {
                                 
                                 // If the item already exists, update the quantity by 1
                                     const updatedCart = state.cart.map((cartItem) => {
                                         if (cartItem.id === item.id) {
-                                            return {
-                                                ...cartItem,
-                                                quantity: cartItem.quantity! + 1
-                                            }
+                                            return { ...cartItem, quantity: cartItem.quantity! + 1 }
                                         }    
                                         return cartItem;
-                                    })
+                            })
                                     return {cart: updatedCart}
                                     // Else keep everything the way it was,add the new item to the cart
                                 } else {
-                                    return {
-                                        cart: [...state.cart, {...item, quantity: 1}]
-                                            }
-                                        }
-                                    
-                }),
-                // Remove Item from Cart
-                removeProduct: (item) => set((state) => {
-
-                    // Check if the item already exists in the cart & remove quantity -1
-                    const existingItem = state.cart.find((cartItem) => cartItem.id === item.id);
-                    // If the item already exists, update the quantity by 1
-                    if (existingItem && existingItem.quantity! > 1) {
-
-                        // If the item already exists, update the quantity by 1
-                        const updatedCart = state.cart.map((cartItem) => {
-                            if (cartItem.id === item.id) {
-
-                                return {
-                                    ...cartItem,
-                                    quantity: cartItem.quantity! - 1
+                                    return {cart: [...state.cart, {...item, quantity: 1}]}
+                                        
                                 }
+              
+                        }),
+                        // item.id IT'S THE ITEM THAT WE ARE PASSING IN DOWN product/addCart.tsx
+                        // by doing this we are making sure that we are passing the same product than we are in  our  state
+                        removeProduct:(item) => set((state) => {
+                            // Check if the item already exists in the cart
+                            const existingItem = state.cart.find((cartItem) => cartItem.id === item.id);
+                            if (existingItem && existingItem.quantity! > 1) {
+                                const updatedCart = state.cart.map((cartItem) => {
+                                    if(cartItem.id === item.id) {
+                                        return {...cartItem, quantity: cartItem.quantity! - 1}
+                                    }
+                                    return cartItem;
+                                })
+                                return {cart: updatedCart}
+                            }else{
+                                // REMOVE ITEM FROM CART
+                                const filteredCart = state.cart.filter((cartItem) => cartItem.id !== item.id)
+                                return {cart: filteredCart}
                             }
-                            return cartItem;
-                        })
-                        return {cart: updatedCart}
-                    } else {
-                        // Remove the item from the cart
-                        const filteredCart = state.cart.filter((cartItem) => cartItem.id !== item.id);
-                        return {cart: filteredCart}
-                    }
-                }),
-                
-                        
-               setPaymentIntent: (val) => set((state) => ({ paymentIntent: val })),
-               setCheckout:      (val) => set((state) => ({ onCheckout: val })),
-                
-        }),
-    { name: "cart-store"}
+                        }),
+         }),
+            { name: "cart-store"}
     )
 )
