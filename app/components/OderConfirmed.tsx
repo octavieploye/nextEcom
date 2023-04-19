f90118e77d9568fc0fd8d52097dd94c8d7065155
octavieploye <octavie.ploye@gmail.com>
2023-04-20 01:22:58 +0200

    clearCartpart 1 OK

--- app/components/OderConfirmed.tsx
+++ app/components/OderConfirmed.tsx
@@ -5,9 +5,14 @@ import Image from 'next/image'
 import cool from '@/public/cool.gif'
 import Link from 'next/link'
 import { useCartStore } from '@/store'
+import { useEffect } from 'react'
 
 export default function OrderConfirmed() {
-    const cartStore = useCartStore()
+   const cartStore = useCartStore()
+   useEffect(() => {
+    cartStore.setPaymentIntent('')
+   },[])
+ 
     return (
         <motion.div  className='flex items-center justify-center my-12'  initial={{ scale: 0.5, opacity: 0}}  animate={{scale: 1, opacity: 1}} >
             <div className='p-12 rounded-md text-center'>
@@ -15,17 +20,15 @@ export default function OrderConfirmed() {
                 <h2 className='text-sm my-4 ' >Check your inbox for the receipt</h2>
                 <Image src={cool} className='py-8 rounded-full' alt='cool gorilla with headphones'/>
                     <div className='flex items-center justify-center gap-12' >
-                        <Link  href={'/dashboard'}>
-                            <button  className='font-medium' >Check your Order</button>
-                        </Link>
-                        <button onClick={() => {
-                            setTimeout(() => {
-                                cartStore.setCheckout('cart')
-                                cartStore.toggleCart()
-                                
-                            })
-                        }} >Create a New Order
-                        </button>
+                      <Link href={"/dashboard"}>
+                      <button className='font-medium'>Check your Order</button>
+                      </Link>
+                      <button onClick={() => {
+                            cartStore.setCheckout('cart')
+                            cartStore.toggleCart()
+                      }}>
+                            Continue Shopping
+                      </button>
                     </div>
             </div>
         </motion.div>
