0d3006af407e8412893d0c4326ed9bedcd6ead00
octavieploye <octavie.ploye@gmail.com>
2023-05-12 16:24:08 +0200

    ready to commit - git cache removed

--- Utility/PriceFormat.ts
+++
@@ -1,8 +0,0 @@
-const formatPrice = (amount: number) => {
-    return new Intl.NumberFormat('fr-FR', {
-        style: 'currency',
-        currency: 'EUR',
-    }).format(amount / 100);
-}
-
-export default formatPrice;
--- type/AddCartType.ts
+++
@@ -1,7 +0,0 @@
-export type AddCartType = {
-    id:string
-    name: string;
-    image: string;
-    quantity?: number | 1;
-    unit_amount: number | null;
-}
